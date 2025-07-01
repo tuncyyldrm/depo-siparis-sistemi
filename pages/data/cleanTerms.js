dafef4978bcd898443973c643d82674c51ff96c0
Tuncay Yıldırım <tuncyyldrm@gmail.com>
2025-07-01 11:10:35 +0300

    Create cleanTerms.js

---
+++ pages/data/cleanTerms.js
@@ -0,0 +1,14 @@
+// data/cleanTerms.js
+const cleanTerms = [
+  "BSCH",
+  "FEBI/SWAG",
+  "ACD",
+  "ACHR",
+  "ACORN",
+  "AEM",
+  "AFK",
+  "AKS",
+  // ... listeyi buraya devam ettir
+];
+
+export default cleanTerms;
