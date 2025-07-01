46aff62ea59717779c9261b24aface4d2d129880
Tuncay Yıldırım <tuncyyldrm@gmail.com>
2025-07-01 11:20:39 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 // pages/_app.js
 import '../styles/globals.css';
-import cleanTerms from '../data/cleanTerms';
-
-
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
