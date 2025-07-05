34a62ebe2e9a04ccdb4f85b01cbebb585d1766db
Tuncay Yıldırım <tuncyyldrm@gmail.com>
2025-07-05 09:54:49 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 // pages/_app.js
+import Head from 'next/head';
 import '../styles/globals.css';
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0d47a1" />
+        <link rel="apple-touch-icon" href="/placeholder-image.png" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Depo Sipariş" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
