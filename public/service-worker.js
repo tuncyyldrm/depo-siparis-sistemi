73086dece13bfc18fa6846b52812d55d253b2072
Tuncay Yıldırım <tuncyyldrm@gmail.com>
2025-07-31 10:20:53 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,15 +27,16 @@ self.addEventListener('notificationclick', event => {
   event.notification.close();
 
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
-      for (const client of windowClients) {
-        if (client.url === event.notification.data.url && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(event.notification.data.url);
-      }
-    })
+clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+  for (const client of windowClients) {
+    if (client.url === event.notification.data.url && 'focus' in client) {
+      return client.focus();
+    }
+  }
+  if (clients.openWindow) {
+    return clients.openWindow(event.notification.data.url);
+  }
+});
+
   );
 });
