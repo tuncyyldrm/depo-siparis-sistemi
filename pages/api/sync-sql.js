cc1026e5b60e533f533b9521c60bd1ef2fc33660
Tuncay Yıldırım <tuncyyldrm@gmail.com>
2025-08-28 13:45:31 +0300

    Update sync-sql.js

--- pages/api/sync-sql.js
+++ pages/api/sync-sql.js
@@ -94,10 +94,7 @@ export default async function handler(req, res) {
         fisno: temizleFisno(order.FISNO),
         carikod: order.STHAR_CARIKOD || null,
         siparis_notu: order.SIPARIS_NOTU || null,
-        created_at: order.SIPARISTARIHI
- 		 ? order.SIPARISTARIHI.replace(' ', 'T') + 'Z'
- 	 	: null
-		  	,
+        created_at: order.SIPARISTARIHI	|| null,
       }))
       .filter((o) => o.fisno);
 
@@ -248,6 +245,7 @@ export default async function handler(req, res) {
     if (pool) await pool.close();
   }
 }
+
 
 
 
